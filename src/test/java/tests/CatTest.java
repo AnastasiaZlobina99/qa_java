57e740e9a39b79d11a67d0796bca8d3c60a32907
Anastasia Zlobina <atasiya.alex@yandex.ru>
2025-04-10 01:52:06 +0700

    Sprint6

---
+++ src/test/java/tests/CatTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+import static org.junit.Assert.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void getSoundTest() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+        Cat cat = new Cat(feline);
+        assertEquals(expectedFood, cat.getFood());
+    }
+
+
+}
